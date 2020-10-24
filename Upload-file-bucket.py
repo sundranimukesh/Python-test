83cfecfc0c8cc8cb16687f20a4c70314ec9b917b
sundranimukesh <35099905+sundranimukesh@users.noreply.github.com>
2020-10-24 14:17:32 +0530

    Update Upload-file-bucket.py

--- Upload-file-bucket.py
+++ Upload-file-bucket.py
@@ -1,14 +1,5 @@
 import boto3
 s3 = boto3.resource('s3')
 
-#Print out bucket names
-#for bucket in s3.buckets.all():
-    #print(bucket.name)
-
-#Upload a new file
 data = open('muk_myfile.txt', 'rb')  # file mode = binary read
-s3.Bucket('advpythonkukesh01').put_object(Key='muk_myfile.txt', Body=data)
-
-# #download file
-# s3client = boto3.client('s3')
-# s3client.download_file('advpythonalisha01', 'muk_myfile.txt)
+s3.Bucket('advpythonmukesh01').put_object(Key='muk_myfile.txt', Body=data)
